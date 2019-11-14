4226573b5152470cd465df7033b4b566cdba2c6c
Audrey MARTIN <audrey.martin26@gmail.com>
2019-11-14 10:25:52 +0100

    tp 1 encapsulation

---
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,19 @@
+package fr.diginamic.banque;
+
+import fr.diginamic.banque.entites.Compte;
+
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Compte c1 = new Compte(14785212,1500.23F);
+		c1.setSoldeCompte(1625.23F);
+		
+		System.out.println("Numero de compte : "+c1.getNumCompte());
+		System.out.println("Solde de compte : "+c1.getSoldeCompte());
+		
+	}
+
+}
---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,32 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+	private int numCompte =0;
+	private float soldeCompte=0.0F;
+	
+
+	public Compte(int pNumCompte, float pSoldeCompte) {
+		this.numCompte = pNumCompte;
+		this.soldeCompte = pSoldeCompte;
+	}
+	
+	
+	public int getNumCompte() {
+		return numCompte;
+	}
+
+	public void setNumCompte(int numCompte) {
+		this.numCompte = numCompte;
+	}
+
+
+	public float getSoldeCompte() {
+		return soldeCompte;
+	}
+
+	public void setSoldeCompte(float soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+
+}
