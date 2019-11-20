2d3c124fdcb627723c102fdf9903967f1dc5c78f
Audrey MARTIN <audrey.martin26@gmail.com>
2019-11-20 12:05:24 +0100

    tp : date et calendar

---
+++ src/main/java/fr/diginamic/dates/TestCalendar.java
@@ -0,0 +1,35 @@
+package fr.diginamic.dates;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class TestCalendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.set(Calendar.YEAR,cal.get(Calendar.YEAR ));
+		cal.set(Calendar.MONTH, cal.get(Calendar.MONTH ));
+		cal.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH ));
+		cal.set(Calendar.HOUR_OF_DAY, 23);
+		cal.set(Calendar.MINUTE, 30);
+		cal.set(Calendar.SECOND, 59);
+		
+		Date date = cal.getTime();
+				
+		SimpleDateFormat formattage = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Date du jour format jour/mois/année\n"+formattage.format(date)+"\n");
+		
+		SimpleDateFormat formattageYearHour = new SimpleDateFormat("yyy/MM/dd HH:mm:ss");
+		System.out.println("Date du jour format année/mois/jour heure:minute:seconde\n"+formattageYearHour.format(date)+"\n");
+		
+		SimpleDateFormat formattageHeure = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		System.out.println("Date du jour format jj-mm-aaaa hh:mm:ss\n"+formattageHeure.format(date)+"\n");
+		
+		
+	}
+
+}
---
+++ src/main/java/fr/diginamic/dates/TestDates.java
@@ -0,0 +1,26 @@
+package fr.diginamic.dates;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestDates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Date date = new Date();
+		date.setHours(23);
+		date.setMinutes(30);
+		date.setSeconds(59);
+		
+		SimpleDateFormat formattage = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Date du jour format jour/mois/année\n"+formattage.format(date)+"\n");
+	
+	    SimpleDateFormat formattageYearHour = new SimpleDateFormat("yyy/MM/dd HH:mm:ss");
+		System.out.println("Date du jour format année/mois/jour heure:minute:seconde\n"+formattageYearHour.format(date)+"\n");
+		
+		SimpleDateFormat formattageHeure = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		System.out.println("Date du jour format jj-mm-aaaa hh:mm:ss\n"+formattageHeure.format(date)+"\n");
+	}
+}
